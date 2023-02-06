96945b6fa28316074ab5d4e55352053dd9b2bab7
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2023-02-06 14:23:28 +0300

    created routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 //imports
 const express = require('express')
 const mongoose = require('mongoose')
+const routes = require('./routes/ToDoRoute')
 
 //initialization
-
 require('dotenv').config()
 
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=> {
+    console.log('connected to MONGODB successfully')
+})
+.catch((err)=> console.log(err))
+
+app.use(routes)
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
-
 //middlewares
