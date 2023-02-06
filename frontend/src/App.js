683ac27029415a8aa9906660bd83512a77b64d5b
ISAAC WAMBIRI <isaacwambiri254@gmail.com>
2023-02-06 16:33:54 +0300

    intialized the fronted

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1> TODO APP </h1>
+        <div className='top'>
+         <input type="text" placeholder='enter new ToDo' />
+
+        <div className='add'>Add</div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
